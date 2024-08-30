84b3d78b9f78e6207b5bfc47fdf4db76cc19c1ad
cstile2 <cstile2@illinois.edu>
2024-08-29 22:16:49 -0500

    added

--- main.js
+++ main.js
@@ -1,33 +1,36 @@
-let list_of_songs = [ "audio/hyperreal.wav", "audio/Say.wav" ];
-let song_titles = ["New Planet", "Say"]
-let song_artists = ["Colsen", "Colsen"]
-let song_covers = ["images/red.jpg","images/basement.jpg"]
+let list_of_songs = [ "audio/hyperreal.wav", "audio/Say.wav", "audio/guitar.wav" ];
+let song_titles = ["New Planet", "Say", "Guitar Sound"]
+let song_artists = ["Colsen", "Colsen", "IDK"]
+let song_covers = ["images/red.jpg","images/basement.jpg", "images/nectar.jpg"]
 let current_song = 0;
-let playing = false; 
+let playing = false;
 let id;
-let r = document.querySelector(':root'); // r.style.setProperty('--blue', 'lightblue');
+let r = document.querySelector(':root');
 let slider = document.getElementById("pro_bar");
 let data_not_loaded = false;
 
+let want_to_play = false;
+
 document.getElementById("main_audio_player").src = list_of_songs[0];
 
-function TimeUpdate()
-{
-    if (!data_not_loaded) {
-        slider.value = (document.getElementById("main_audio_player").currentTime / document.getElementById("main_audio_player").duration) * 100.0;
-    }
-    else {
-        slider.value = 0.0;
+function TimeUpdate() {
+    if(!data_not_loaded){    
+    slider.value = (document.getElementById("main_audio_player").currentTime / document.getElementById("main_audio_player").duration) * 100.0;
     }
     PaintSliderTrack();
 }
-function MetaDataLoaded() {
+function OnCanPlay() {
     data_not_loaded = false;
-    TimeUpdate();
+    r.style.setProperty('--colo', 'white');
+    if(want_to_play) {
+        PlayMusic();
+    }
+    want_to_play = false;
 }
 function BarInput() {
     document.getElementById("main_audio_player").currentTime = (slider.value / 100.0) * document.getElementById("main_audio_player").duration;
-    TimeUpdate();
+    // TimeUpdate();
+    PaintSliderTrack();
 }
 function PaintSliderTrack() {
     const sliderValue = slider.value;
@@ -36,17 +39,16 @@ function PaintSliderTrack() {
 function MusicEnded() {
     PauseMusic();
 }
-
-function PlayCurrentSong() {
+function StartCurrentSong() {
     document.getElementById("main_audio_player").src = list_of_songs[current_song];
-    document.getElementById("main_audio_player").currentTime = 0;
 
     data_not_loaded = true;
+    r.style.setProperty('--colo', 'gray');
 
-    PlayMusic();
+    want_to_play = true;
+    document.getElementById("main_audio_player").load();
     UpdateMediaPlayer();
 }
-
 function PlayMusic()
 {
     document.getElementById("main_audio_player").play();
@@ -58,23 +60,21 @@ function PauseMusic() {
     document.getElementById("play_button_path").setAttribute("d", " M 0 64 C 0 28.677 28.677 0 64 0 C 99.323 0 128 28.677 128 64 C 128 99.323 99.323 128 64 128 C 28.677 128 0 99.323 0 64 Z  M 107.074 64.589 L 73.037 84.24 L 39 103.891 L 39 64.589 L 39 25.287 L 73.037 44.938 L 107.074 64.589 Z " );
     playing = false;
 }
-function NextSong()
-{
+function NextSong() {
     current_song += 1;
     if (current_song >= list_of_songs.length) {
         current_song = 0;
     }
     
-    PlayCurrentSong();
+    StartCurrentSong();
 }
-function PreviousSong()
-{
+function PreviousSong() {
     current_song -= 1;
     if (current_song < 0) {
         current_song = list_of_songs.length - 1;
     }
     
-    PlayCurrentSong();
+    StartCurrentSong();
 }
 
 // GUI
@@ -87,7 +87,7 @@ document.getElementById("play_button_square").addEventListener("click", function
 });
 document.getElementById("skip_back").addEventListener("click", PreviousSong);
 document.getElementById("skip_forward").addEventListener("click", NextSong);
-document.addEventListener("resize", (event) => {});
+// document.addEventListener("resize", (event) => {});
 document.addEventListener("keydown", (event) => {
     if(event.key == ' ') {
         if (playing) {
@@ -110,17 +110,18 @@ function UpdateMediaPlayer() {
     document.getElementById("cover").src = song_covers[current_song];
 }
 
-for (let i = 0; i < song_covers.length; i++){
-    document.getElementById("songlist").innerHTML += `<div class="row" role="button" data-track="`+i+`">
+for (let i = 0; i < song_covers.length; i++) {
+    document.getElementById("songlist").innerHTML += `<div class="row" data-track="`+i+`">
           <img src="`+song_covers[i]+`" class="thumbnail"></img>
           <div class="rowtext"><div class="title">`+song_titles[i]+`</div><div class="artist">`+song_artists[i]+`</div></div>
         </div>`
+    
 }
 let rows = document.getElementsByClassName("row");
 for(let i = 0; i < rows.length; i++) {
     rows[i].addEventListener("click", function() {
         current_song = Number(rows[i].dataset.track);
-        PlayCurrentSong();
+        StartCurrentSong();
     });
 }
 
